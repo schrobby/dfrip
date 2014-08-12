787bb632184032320b074ff159ecd67ca07f64b8
schrobby <schrawby@gmail.com>
2014-08-13 01:09:54 +0200

    Implemented full CLI, SRT conversion, more

--- dfrip.py
+++ dfrip.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
+from os import path
+from bs4 import BeautifulSoup
 import requests
 import json
-from os import path
-import sys
+import re
 
 API_BASE = "http://www.dramafever.com/api/4"
 API_CS_V4 = "DA59dtVXYLxajktV"
@@ -30,6 +32,10 @@ def get_subs_url(series_id, episode_num, lang="en", format="dfxp", file_format="
         lang=lang, format=format, file_format=file_format)
     return response["url"]
 
+def download_subs(url):
+    r = requests.get(url)
+    return str(r.content)
+
 def download_file_to(url, file_path):
     r = requests.get(url, stream=True)
     with open(file_path, "wb") as f:
@@ -39,16 +45,68 @@ def download_file_to(url, file_path):
                 f.flush()
     return file_path
 
+def save_to_disk(filename, content):
+    with open(filename, "w") as f:
+        f.write(content)
+
+def xml_to_srt(xml_subs):
+    # XML to SRT Python implementation inspired by Hubwub's PyXMLtoSRT
+    # https://github.com/hubwub/PyXMLtoSRT/
+
+    xml_subs = re.sub(r"\n +", "\n", xml_subs)
+    soup = BeautifulSoup(xml_subs)
+
+    parse_time = lambda time: time.replace('.', ',')
+    srt = ''
+
+    for div_tag in soup.findAll('div'):
+        for i, p_tag in enumerate(div_tag.findAll('p')):
+            
+            srt += str(i + 1) + '\n'
+            srt += "%s --> %s\n" % (parse_time(p_tag.get('begin')), parse_time(p_tag.get('end')))
+
+            line = p_tag.text.strip('\n')
+            if "<i>" in line and not "</i>" in line:
+                line += "</i>"
+
+            srt += line + '\n\n'
+
+    return srt
+
 if __name__ == "__main__":
-    series = sys.argv[1]
-    episode = sys.argv[2]
+    import argparse
 
-    url = get_subs_url(series, episode)
-    print("Series: " + get_series(series)["name"])
-    print("URL: " + url)
-    print("Saving file as " + url.split('/')[-1])
+    parser = argparse.ArgumentParser(description="Download subtitles from DramaFever and convert them into TTML or SRT.", \
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("series_id", metavar="SERIES", type=int, help="the ID of the series (usually four-digit number)")
+    parser.add_argument("episode_num", metavar="EPISODE", type=int, help="the number of the episode")
+    parser.add_argument("-f", "--format", choices=["srt", "ttml", "xml"], default="xml", help="the format the subtitles will be saved in")
+    parser.add_argument("-o", "--out", metavar="FILENAME", default=".", help="location of the output file or filename")
+    parser.add_argument("-v", "--verbose", action="store_true", help="increases output verbosity")
 
-    download_file_to(url, url.split('/')[-1])
+    args = parser.parse_args()
 
+    try:
+        series = get_series(args.series_id)
+        subs_url = get_subs_url(args.series_id, args.episode_num)
+    except ValueError, e:
+        print("Error: " + str(e))
+        quit()
 
+    print("Series: %s, %s" % (series["name"], series["native_lang_title"]))
+    if args.verbose:
+        print("Description: " + series["description_short"])
+        print("URL: http://www.dramafever.com" + series["www_url"])
 
+    print("\nDownloading subtitle file ...")
+    subs = download_subs(subs_url)
+    filename = subs_url.split('/')[-1]
+    if args.format == "srt": 
+        subs = xml_to_srt(subs)
+        filename = filename.split('.')[0] + ".srt"
+
+    if args.out:
+        filename = args.out + '/' + filename if path.isdir(args.out) else args.out
+
+    print("Saving file as " + filename)
+    save_to_disk(filename, subs) 
